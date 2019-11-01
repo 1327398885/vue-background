c49248aeb3bc9452acb10db8d4d324bd786c0bdb
sungang <1327398885@qq.com>
2019-11-01 11:42:49 +0800

    新建vue-admin的后台项目

---
+++ src/main/java/com/sun/vuebackground/VueBackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.sun.vuebackground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueBackgroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sun/vuebackground/VueBackgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.vuebackground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackgroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
